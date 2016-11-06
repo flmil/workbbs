5018ee90414b8562c66e4dee9ed4a14fbac2b01c
ojirei <Rei@ojirei-no-MacBook-Pro.local>
2016-11-06 16:01:35 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,88 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'open-uri'
+require 'sinatra/json'
+require './models/contribution'
+
+get '/' do
+	@contents = Contribution.all
+	erb :index
+end
+
+post '/new' do
+	Contribution.create({
+		name: params[:name],
+		address: params[:address],
+		traffic: params[:traffic],
+		money: params[:money],
+		shift: params[:shift],
+		explanation: params[:explanation],
+		feature: params[:feature],
+		condition: params[:condition],
+		period: params[:period],
+		count: params[:count],
+		fare: params[:fare],
+		treatment: params[:treatment],
+		ratio: params[:ratio],
+		age: params[:age],
+		atmosphere: params[:atmosphere],
+		message: params[:message],
+		manner: params[:manner],
+		flow: params[:flow],
+		tel: params[:tel]
+	})
+
+	redirect '/'
+end
+
+post '/details' do
+	Contribution.find(paramus[:id])
+	erb :details
+end
+
+post '/renew/:id' do
+	@content = Contribution.find(params[:id])
+	@content.update({
+		name: params[:name],
+		address: params[:address],
+		traffic: params[:traffic],
+		money: params[:money],
+		shift: params[:shift],
+		explanation: params[:explanation],
+		feature: params[:feature],
+		condition: params[:condition],
+		period: params[:period],
+		count: params[:count],
+		fare: params[:fare],
+		treatment: params[:treatment],
+		ratio: params[:ratio],
+		age: params[:age],
+		attribute: params[:attribute],
+		atmosphere: params[:atmosphere],
+		message: params[:message],
+		manner: params[:manner],
+		flow: params[:flow],
+		tel: params[:tel],
+	})
+
+	redirect '/'
+end
+
+
+#get '/'do
+#	@names = name.all
+#end
+
+#get 'show/:name_id' do
+#	@name = name.find_by(id: name_id)
+#end
+
+#get '/' do
+#	@names = name.all
+#end
+
+#get 'show/:name_id' do
+#	@name = name.find_by(id: name_id)
+#end
+
---
+++ db/migrate/20161015052057_create_contributions.rb
@@ -0,0 +1,26 @@
+class CreateContributions < ActiveRecord::Migration
+  def change
+	  create_table :contributions do |t|
+		  t.string :name
+		  t.string :address
+		  t.string :traffic
+		  t.string :money
+		  t.string :shift
+		  t.string :explanation
+		  t.string :feature
+		  t.string :condition
+		  t.string :period
+		  t.string :count
+		  t.string :fare
+		  t.string :treatment
+		  t.string :ratio
+		  t.string :age
+		  t.string :atmosphere
+		  t.string :message
+		  t.string :manner
+		  t.string :flow
+		  t.string :tel
+		  t.timestamps null: false
+	  end
+  end
+end
