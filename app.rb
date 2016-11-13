16e94df7014dc14ca3603a78f389d4b92c852879
JpnLavender <nagisa20000014@gmail.com>
2016-11-13 20:20:45 +0900

    helperの追加と、バグ？修正の追いついてないものを修正

--- app.rb
+++ app.rb
@@ -5,72 +5,52 @@ require 'open-uri'
 require 'sinatra/json'
 require './models/contribution'
 
+helpers do
+  def contribution_data
+    {name: params[:name],
+     address: params[:address],
+     traffic: params[:traffic],
+     money: params[:money],
+     shift: params[:shift],
+     explanation: params[:explanation],
+     feature: params[:feature],
+     condition: params[:condition],
+     period: params[:period],
+     count: params[:count],
+     fare: params[:fare],
+     treatment: params[:treatment],
+     ratio: params[:ratio],
+     age: params[:age],
+     representations: params[:representations],
+     atmosphere: params[:atmosphere],
+     message: params[:message],
+     manner: params[:manner],
+     flow: params[:flow],
+     tel: params[:tel]}
+  end
+end
+
 get '/' do
-	@contents = Contribution.all
-	erb :index
+  @contents = Contribution.all
+  erb :index
 end
 
 post '/new' do
-	Contribution.create({
-		name: params[:name],
-		address: params[:address],
-		traffic: params[:traffic],
-		money: params[:money],
-		shift: params[:shift],
-		explanation: params[:explanation],
-		feature: params[:feature],
-		condition: params[:condition],
-		period: params[:period],
-		count: params[:count],
-		fare: params[:fare],
-		treatment: params[:treatment],
-		ratio: params[:ratio],
-		age: params[:age],
-		representations: params[:representations],
-		atmosphere: params[:atmosphere],
-		message: params[:message],
-		manner: params[:manner],
-		flow: params[:flow],
-		tel: params[:tel]
-	})
+  Contribution.create(contribution_data)
 
-	redirect '/'
+  redirect '/'
 end
 
 get '/details/:content_id' do
-	@content = Contribution.all
-#	@content = content.find_by(id: content_id)
-#	Contribution.find(paramus[:id])
-	erb :details
+  @content = Contribution.find_by(id: params[:content_id])
+  erb :details
 end
 
 
-#post '/renew/:id' do
-#	@content = Contribution.find(params[:id])
-#	@content.update({
-#		name: params[:name],
-#		address: params[:address],
-#		traffic: params[:traffic],
-#		money: params[:money],
-#		shift: params[:shift],
-#		explanation: params[:explanation],
-#		feature: params[:feature],
-#		condition: params[:condition],
-#		period: params[:period],
-#		count: params[:count],
-#		fare: params[:fare],
-#		treatment: params[:treatment],
-#		ratio: params[:ratio],
-#		age: params[:age],
-#		attribute: params[:attribute],
-#		atmosphere: params[:atmosphere],
-#		message: params[:message],
-#		manner: params[:manner],
-#		flow: params[:flow],
-#		tel: params[:tel],
-#	})
-
-#	redirect '/'
+# post '/renew/:id' do
+# 	@content = Contribution.find(params[:id])
+# 	@content.update(contribution_data)
+#	  redirect '/'
 #end
 
 
